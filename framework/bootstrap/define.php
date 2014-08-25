101638272524f855dac0bc915d8537c688c4a083
MRodriguez08 <mau.rod.81090@gmail.com>
2014-08-25 18:08:04 -0300

    autoinferred application base path

--- framework/bootstrap/define.php
+++ framework/bootstrap/define.php
@@ -1,13 +1,38 @@
 <?php
 
 /**
- * archivo para la definicion de constantes de la aplicacion
- *  - url para autoloader
- *  - url a controladores
- *  - etc..
+ * Application global constants definition file.
+ * Here are defined>
+ *  - autoloader url
+ *  - controller's directory definition
+ *  - others..
  */
 
-define('__BASE_PATH', '/opt/lampp/htdocs/simple-php-mvc-framework');
+/**
+ * Current script directory to infer other directories.
+ */
+$scrUrl = dirname($_SERVER['PHP_SELF']);
+$scrPath = __FILE__;
+/**
+ * Base path of the application on the filesystem
+ */
+$parts =  explode(DIRECTORY_SEPARATOR , $scrPath);
+$baseArr = array_slice($parts, 0, count($parts) - 3);
+$basePath = join(DIRECTORY_SEPARATOR, $baseArr);
+define('__BASE_PATH', $basePath);
 
+/**
+ * Base url of the application in the web server
+ */
+$parts =  explode(DIRECTORY_SEPARATOR , $scrUrl);
+define('__BASE_URL','/' . $parts[1]);
+
+/**
+ * Application root directory
+ */
 define('__APP_PATH', __BASE_PATH . "/app");
+
+/**
+ * Application controller's directory
+ */
 define('__CONTROLLERS_PATH', __APP_PATH . "/controllers");
