fc421b7e35694e2f38776939b9aa9fd83b0cfba6
MRodriguez08 <mau.rod.81090@gmail.com>
2014-08-25 19:51:32 -0300

    added sbadmin2 assets

---
+++ app/views/sbadmin2/partial2.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @masterPage=sbadmin2MasterPage
+ * @viewType=View
+ */
+?>
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Partial View 2</h1>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
